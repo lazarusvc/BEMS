f21acd1530d84cda974346c41158b360de1c2f9f
Kelvin K. Woodman <kwoodman@gmail.com>
2022-02-21 22:57:17 -0400

    Budget Data Entry Intiaal screen prototyped

--- DataAccess/Models/ProcessingYearModel.cs
+++ DataAccess/Models/ProcessingYearModel.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace DataAccessLibrary.Models
 {
-    public class ProcessingYearModel
+    public  class  ProcessingYearModel
     {
           public int year { get; set; }
           public bool ready_for_data_entry { get; set; }
 
-        
+        public string displayYear { get { return (year + " - " + (year + 1)); } }
     }
 }
